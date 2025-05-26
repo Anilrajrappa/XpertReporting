5485e2c751f6d424cd6b06e454bdc77f57e02607
ANILRAJRAPPA <S123@@SSPLDEV>
2025-05-26 15:09:26 +0530

    Anil

--- Form1.vb
+++ Form1.vb
@@ -9,7 +9,7 @@
         DataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells
 
         ' Optionally, you can set the column width if you want
-        DataGridView1.Columns("Column2").Width = 100
+        DataGridView1.Columns("Column2").Width = 150
 
         'Rohit : 26-05-2025 Testing foor GIT HUB
 
